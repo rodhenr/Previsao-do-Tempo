b7a8b25f2b2266dd233846ed4051108ca284c174
Rodrigo Henrique <rodhenrc@outlook.com>
2022-05-07 11:08:01 -0300

    Base do projeto feita

---
+++ src/components/Key.tsx
@@ -0,0 +1,3 @@
+const key = "75da883c8cc9404ea1d123710220705";
+
+export default key;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useState } from "react";
+import axios from "axios";
+import Key from "./Key.tsx";
+import "../styles/Search.css";
 
 function Search() {
+  const [cidade, setCidade] = useState("");
+  const [dados, setDados] = useState();
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=${Key}&q=${cidade}&days=6&aqi=no&alerts=no`;
+
+  function handleSearch() {
+    const call = axios
+      .get(url)
+      .then((response) => setDados(response.data.forecast.forecastday));
+  }
+
   return (
-    <div>Search</div>
-  )
+    <div>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Digite a cidade"
+          onChange={(e) => setCidade(e.target.value)}
+        />
+        <button onClick={handleSearch}>Procurar</button>
+      </div>
+      <div className="forecast-container">
+        {dados ? (
+          <>
+            <div className="forecast-cidade">
+              <p>Previsão para {cidade.toUpperCase()} nos próximos dias</p>
+            </div>
+            <div className="forecast">
+              {dados.map((i, key) => (
+                <div key={key} className="forecast-day">
+                  <img src={i.day.condition.icon} alt="temp" />
+                  <p>{i.date}</p>
+                  <div className="forecast-temp">
+                    <span>MIN: {i.day.mintemp_c}°C</span>
+                    <span>MAX: {i.day.maxtemp_c}°C</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
